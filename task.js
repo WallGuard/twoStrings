6ba8ed24fe81303b0dc1d9e6ca693cdbf4029a97
Wallguard <wallguard@localhost.localdomain>
2020-02-04 16:38:25 +0300

    Intial Commit

---
+++ task.js
@@ -0,0 +1,10 @@
+function checkBlock(a, b) {
+    return !((a === '') || (b === '') || (a.length !== b.length) || (a === b))
+  }
+
+function main (a, b) {
+    let a = a.split('');
+    let b = b.split('');
+
+    
+}
